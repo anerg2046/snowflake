3f433e88ae853dba39d14888d197d6e547ac0299
Coeus <r.anerg@gmail.com>
2018-07-30 11:19:56 +0800

    first

---
+++ README.md
@@ -0,0 +1,40 @@
+# snowflake
+====
+
+## 用法
+默认有3个参数可以进行设置
+- Epoch 设置一个小于当前时间的毫秒数，用于缩短时间位数
+- MaxMachineBit 最大的机器标识bit位数，默认是8，取值范围1-255，如果为0会忽略机器标识这一段
+- MaxSequenceBit 每毫秒内的流水号bit位数，默认是12，取值范围是0-4095
+
+···go
+package main
+
+import (
+	"fmt"
+
+	"github.com/anerg2046/snowflake"
+)
+
+func main() {
+	//以下3个参数为可选
+	snowflake.Epoch = 1532620800000
+	snowflake.MaxMachineBit = 10
+	snowflake.MaxSequenceBit = 10
+	//创建一个机器ID的节点，这里是1，如果为0则会忽略机器标识这一段
+	node, err := snowflake.NewNode(1)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	//获取一个全局唯一的ID
+	id := node.NextID()
+	//输出为int64类型
+	fmt.Println(id.Int64())
+	//输出为string类型
+	fmt.Println(id.String())
+	//输出为Base36类型，主要是缩短位数
+	fmt.Println(id.Base36())
+
+}
+```
---
+++ snowflake.go
@@ -0,0 +1,75 @@
+package snowflake
+
+import (
+	"errors"
+	"strconv"
+	"strings"
+	"sync"
+	"time"
+)
+
+var (
+	// Epoch 开始时间戳，固定小于当前时间的毫秒，用于缩减时间位数
+	Epoch int64 = 1532620800000
+	// MaxMachineBit 最大机器标识位数，8->255 10->1023
+	MaxMachineBit uint8 = 8
+	// MaxSequenceBit 最大序号数位数，12->4095
+	MaxSequenceBit uint8 = 12
+	// maxMachineID 最大机器标识号
+	maxMachineID uint16 = (1 << MaxMachineBit) - 1
+	// timestamp 毫秒时间戳
+	timestamp int64
+	// sequence 每毫秒内的队列
+	sequence int64
+)
+
+type Node struct {
+	mu        sync.Mutex
+	machineID uint16
+}
+type ID int64
+
+func NewNode(machineID uint16) (*Node, error) {
+	maxMachineID = (1 << MaxMachineBit) - 1
+	if machineID > maxMachineID {
+		return nil, errors.New("Machine ID must be between 0 and " + strconv.FormatInt(int64(maxMachineID), 10))
+	}
+	return &Node{machineID: machineID}, nil
+}
+
+func (n *Node) NextID() ID {
+	n.mu.Lock()
+
+	now := time.Now().UnixNano() / 1000000
+	if now == timestamp {
+		sequence++
+	} else {
+		timestamp = now
+		sequence = 0
+	}
+
+	timeDiff := now - Epoch
+	var nextID ID
+	if n.machineID == 0 {
+		timeLeftShift := MaxSequenceBit
+		nextID = ID((timeDiff << timeLeftShift) | sequence)
+	} else {
+		timeLeftShift := MaxSequenceBit + MaxMachineBit
+		nextID = ID((timeDiff << timeLeftShift) | (int64(n.machineID) << MaxSequenceBit) | sequence)
+	}
+
+	n.mu.Unlock()
+	return nextID
+}
+
+func (i ID) Int64() int64 {
+	return int64(i)
+}
+
+func (i ID) String() string {
+	return strconv.FormatInt(int64(i), 10)
+}
+
+func (i ID) Base36() string {
+	return strings.ToUpper(strconv.FormatInt(int64(i), 36))
+}
